bfd9fb5bfff17a9584daaa75d3204881a84c21c7
Ector Cunha <ectorvccunha@outlook.com>
2023-04-18 17:36:14 -0300

    initial commit

---
+++ src/botPage/view/deriv/layout/Header/Header.jsx
@@ -0,0 +1,227 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import classNames from 'classnames';
+import { isMobile, isDesktop, parseQueryString } from '../../../../../common/utils/tools';
+import PlatformDropdown from './components/platform-dropdown.jsx';
+import { isLoggedIn, getActiveToken, updateTokenList } from '../../utils';
+import {
+    getTokenList,
+    removeAllTokens,
+    syncWithDerivApp,
+    set as setStorage,
+    get as getStorage,
+} from '../../../../../common/utils/storageManager';
+import {
+    updateIsLogged,
+    resetClient,
+    updateActiveAccount,
+    updateBalance,
+    updateActiveToken,
+    updateAccountType,
+} from '../../store/client-slice';
+import { setAccountSwitcherLoader, updateShowMessagePage } from '../../store/ui-slice';
+import { DrawerMenu, AuthButtons, AccountActions, MenuLinks, AccountSwitcherLoader } from './components';
+import { queryToObjectArray } from '../../../../../common/appId';
+import api from '../../api';
+import config from '../../../../../app.config';
+import { observer as globalObserver } from '../../../../../common/utils/observer';
+import { checkSwitcherType } from '../../../../../common/footer-checks';
+
+// [Todo] We will update this during the API improvement process
+let is_subscribed = false;
+const AccountSwitcher = () => {
+    const { account_switcher_loader } = useSelector(state => state.ui);
+    const { is_logged } = useSelector(state => state.client);
+    const query_string = parseQueryString();
+    const query_string_array = queryToObjectArray(query_string);
+
+    // [Todo] We should remove this after update the structure of get token list on login
+    if (query_string_array[0]?.token) {
+        return (
+            <div className='header__menu-right-loader'>
+                <AccountSwitcherLoader />
+            </div>
+        );
+    }
+    if (account_switcher_loader) {
+        return (
+            <div className='header__menu-right-loader'>
+                <AccountSwitcherLoader />
+            </div>
+        );
+    }
+
+    if (is_logged) return <AccountActions />;
+
+    return <AuthButtons />;
+};
+
+
+const Header = () => {
+    const [isPlatformSwitcherOpen, setIsPlatformSwitcherOpen] = React.useState(false);
+    const [showDrawerMenu, updateShowDrawerMenu] = React.useState(false);
+    const platformDropdownRef = React.useRef();
+    const { is_logged, active_token } = useSelector(state => state.client);
+    const { is_bot_running } = useSelector(state => state.ui);
+    const is_logged_in = isLoggedIn();
+    const dispatch = useDispatch();
+    const hideDropdown = e => !platformDropdownRef.current.contains(e.target) && setIsPlatformSwitcherOpen(false);
+
+    React.useEffect(() => {
+        const mountSwitcher = async () => {
+            const res = await checkSwitcherType()
+                .then(data => {
+                    dispatch(updateAccountType(data));
+                })
+                .catch(error => globalObserver.emit('Error', error));
+            return res;
+        };
+        if (is_logged_in) {
+            mountSwitcher();
+        }
+    }, [is_logged_in]);
+
+    React.useEffect(() => {
+        api.onMessage().subscribe(({ data }) => {
+            if (data?.error?.code) return;
+            if (data?.msg_type === 'balance') {
+                dispatch(updateBalance(data.balance));
+            }
+        });
+    }, []);
+
+    React.useEffect(() => {
+        const token_list = getTokenList();
+        const active_storage_token = getActiveToken(token_list);
+        const landing_company = active_storage_token?.loginInfo.landing_company_name;
+        dispatch(updateShowMessagePage(landing_company === 'maltainvest'));
+
+        if (!active_storage_token) {
+            removeAllTokens();
+            dispatch(resetClient());
+            dispatch(setAccountSwitcherLoader(false));
+        }
+        const client_accounts = JSON.parse(getStorage('client.accounts'));
+        const current_login_id = getStorage('active_loginid') || '';
+        const logged_in_token = client_accounts[current_login_id]?.token || active_storage_token?.token || '';
+
+        if (logged_in_token) {
+            api.authorize(logged_in_token)
+                .then(account => {
+                    const active_loginid = account.authorize.account_list;
+                    
+                    active_loginid.forEach(acc => {
+                        if (current_login_id === acc.loginid) {
+                            setStorage('active_loginid', current_login_id);
+                        } else {
+                            setStorage('active_loginid', account.authorize.loginid);
+                        }
+                        updateTokenList();
+                    });
+                    if (account?.error?.code) return;
+                    dispatch(updateActiveToken(logged_in_token));
+                    dispatch(updateActiveAccount(account.authorize));
+                    dispatch(setAccountSwitcherLoader(false));
+                    if (!is_subscribed) {
+                        is_subscribed = true;
+                        api.send({
+                            balance: 1,
+                            account: 'all',
+                            subscribe: 1,
+                        })
+                            .then(({ balance }) => {
+                                globalObserver.setState({
+                                    balance: Number(balance.balance),
+                                    currency: balance.currency,
+                                });
+                            })
+                            .catch(e => {
+                                globalObserver.emit('Error', e);
+                            });
+                    }
+                })
+                .catch(() => {
+                    removeAllTokens();
+                    dispatch(resetClient());
+                    dispatch(setAccountSwitcherLoader(true));
+                });
+            syncWithDerivApp();
+        }
+    }, [active_token]);
+
+    React.useEffect(() => {
+        dispatch(updateIsLogged(isLoggedIn()));
+    }, [is_logged]);
+
+    React.useEffect(() => {
+        window.addEventListener('beforeunload', onBeforeUnload, { capture: true });
+        return () => {
+            window.removeEventListener('beforeunload', onBeforeUnload, { capture: true });
+        };
+    }, [is_bot_running]);
+
+    const onBeforeUnload = e => {
+        if (is_bot_running) {
+            e.preventDefault();
+            e.returnValue = true;
+        }
+    };
+
+    return (
+        <div className='header'>
+            <div id='deriv__header' className='header__menu-items'>
+                {isDesktop() && (
+                    <div className='header__menu-left'>
+                        {isPlatformSwitcherOpen && (
+                            <PlatformDropdown
+                                hideDropdown={hideDropdown}
+                                ref={platformDropdownRef}
+                                setIsPlatformSwitcherOpen={setIsPlatformSwitcherOpen}
+                            />
+                        )}
+                        <div
+                            id='platform__switcher'
+                            className='header__menu-item platform__switcher'
+                            onClick={() => setIsPlatformSwitcherOpen(!isPlatformSwitcherOpen)}
+                        >
+                            <img className='header__logo' src={config.app_logo} />
+                            <div className='platform__switcher-header'>{config.app_title}</div>
+                            <img
+                                id='platform__switcher-expand'
+                                className={classNames('header__icon header__expand', {
+                                    open: isPlatformSwitcherOpen,
+                                })}
+                                src='image/deriv/ic-chevron-down-bold.svg'
+                            />
+                        </div>
+                        {is_logged && <MenuLinks />}
+                    </div>
+                )}
+                {isMobile() && (
+                    <img
+                        className='btn__close header__hamburger'
+                        src='image/deriv/ic-hamburger.svg'
+                        onClick={() => {
+                            updateShowDrawerMenu(true);
+                        }}
+                    />
+                )}
+                <div className='header__menu-right'>
+                    <AccountSwitcher />
+                </div>
+            </div>
+            {showDrawerMenu && (
+                <DrawerMenu
+                    updateShowDrawerMenu={updateShowDrawerMenu}
+                    setIsPlatformSwitcherOpen={setIsPlatformSwitcherOpen}
+                    isPlatformSwitcherOpen={isPlatformSwitcherOpen}
+                    hideDropdown={hideDropdown}
+                    platformDropdownRef={platformDropdownRef}
+                    is_logged={is_logged}
+                />
+            )}
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/botPage/view/deriv/layout/Header/components/tab-content.jsx
@@ -0,0 +1,106 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { translate } from '../../../../../../common/utils/tools';
+import { getTokenList } from '../../../../../../common/utils/storageManager';
+import { useDispatch } from 'react-redux';
+import { setAccountSwitcherToken } from '../../../store/ui-slice';
+import classNames from 'classnames';
+import { observer as globalObserver } from '../../../../../../common/utils/observer';
+import config from '../../../../../../app.config';
+
+const TabContent = ({
+    tab = 'real',
+    isActive,
+    setIsAccDropdownOpen,
+    accounts,
+    title = translate('Deriv account')
+}) => {
+   
+    const [isAccordionOpen, setIsAccordionOpen] = React.useState(true);
+    const dispatch = useDispatch();
+    const { active_account_name } = useSelector(state => state.client);
+    const item_ref = React.useRef([]);
+    const isReal = tab === 'real';
+    const token_list = getTokenList();
+    const onChangeAccount = acc => {
+        const account_token = token_list.find(token => token.accountName === acc);
+        if (account_token?.token && acc !== active_account_name) {
+            dispatch(setAccountSwitcherToken(account_token?.token));
+            setIsAccDropdownOpen(false);
+        }
+    };
+
+    return (
+        <div className={`account__switcher-tabs-content ${isActive ? '' : 'hide'}`}>
+            <div className='account__switcher-accordion'>
+                <h3
+                    className='ui-accordion-header ui-state-default'
+                    onClick={() => setIsAccordionOpen(!isAccordionOpen)}
+                >
+                    <div className='account__switcher-accordion-header-text'>
+                        <span>{title}</span>
+                        <img
+                            className={`header__expand ${isAccordionOpen ? 'open' : ''}`}
+                            src='image/deriv/ic-chevron-down.svg'
+                        />
+                    </div>
+                </h3>
+                <div className={`account__switcher-list ${isAccordionOpen ? 'open' : ''}`}>
+                    {accounts && accounts.sort((acc, acc1) => {
+                                return acc === active_account_name ? -1 : acc1 === active_account_name ? 1 : 0;
+                            })
+                        .map((account, index) => {
+                                const { demo_account, currency, balance } = account;
+                                const currency_icon = demo_account ? 'virtual' : currency?.toLowerCase() || 'unknown';
+                                const getBalance = () => {
+                                    return balance.toLocaleString(undefined, {
+                                        minimumFractionDigits:
+                                            config.currency_name_map[currency]?.fractional_digits ?? 2,
+                                    });
+                                };            
+                                return (
+                                    (isReal) !== Boolean(demo_account) && (
+                                        <div
+                                            className={classNames('account__switcher-acc', {
+                                                'account__switcher-acc--active': active_account_name === account.account,
+                                            })}
+                                            key={account.account}
+                                            onClick={e => {
+                                                e.stopPropagation();
+                                                onChangeAccount(account.account);
+                                            }}
+                                            ref={el => (item_ref.current[index] = el)}
+                                        >
+                                            
+                                            <input type='hidden' name='account_name' value={account.account} />
+                                            <img src={`image/deriv/currency/ic-currency-${currency_icon}.svg`} />
+                                            <span>
+                                                {!currency && (
+                                                    <span className='symbols'>{translate('No currency assigned')}</span>
+                                                )}
+                                                {demo_account
+                                                    ? translate('Demo')
+                                                    : (account.account?.includes('MF') && active_account_name?.includes('MF'))
+                                                    ? 'Multiplers'
+                                                    : config.currency_name_map[currency]?.name || currency}
+
+                                                <div className='account__switcher-loginid'>{account.account}</div>
+                                            </span>
+                                            <span className='account__switcher-balance'>
+                                                {account?.currency && getBalance()}
+                                                <span className='symbols'>
+                                                    &nbsp;
+                                                    {account?.currency === 'UST' ? 'USDT' : account?.currency}
+                                                </span>
+                                            </span>
+                                        </div>
+                                    )
+                                );
+                            })}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default TabContent;
